04f75d56ce894cbb3c0520db980d0d8810630188
Kacy Do <contact@kacydo.dev>
2025-12-16 14:04:09 +1100

    migrate test components to TestPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useEffect } from 'react';
-import { Aside } from './components/Aside';
-import { Main } from './components/Main';
+import { Routes, Route } from 'react-router';
+import { TestPage } from './components/pages/TestPage';
 
 export function App() {
 	useEffect(() => {
-		document.documentElement.setAttribute('data-theme', 'dark');
+		// document.documentElement.setAttribute('data-theme', 'dark');
 		// document.documentElement.setAttribute('class', 'dark');
 	}, []);
 
 	return (
 		<>
-			<Aside />
-			<Main />
+			<Routes>
+				<Route index path='/' element={<>hi</>} />
+
+				<Route path='/test-page' element={<TestPage />} />
+			</Routes>
 		</>
 	);
 }
---
+++ src/components/pages/TestPage.tsx
@@ -0,0 +1,11 @@
+import { Aside } from '../Aside';
+import { Main } from '../Main';
+
+export function TestPage() {
+	return (
+		<>
+			<Aside />
+			<Main />
+		</>
+	);
+}
